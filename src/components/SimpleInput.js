39b49e37572ab1dc4db971ee49302573122f3347
qpakr38 <zczc0819@gmail.com>
2022-09-07 08:51:59 +0900

    FEAT : input 값 가져오기
    1. useState, onChange 기능을 이용하여 변경시마다 값을 저장하는 방법
    2. useRef를 사용하여 Input값을 직접 가져오는 방법
    
    useRef를 사용하여 직접 데이터를 가져오는 것은 괜찮지만 값을 변경하는등의 작업을 할 경우 리액트에서 알수가 없어 생각지 못한 오류를 유발할수 있다.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,37 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const nameInputRef = useRef();
+    const [enteredName, setEnteredName] = useState('');
+
+    const nameInputChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    };
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault();
+        console.log(enteredName)
+        const enteredValue = nameInputRef.current.value;
+        console.log(enteredValue)
+        //DOM객체를 리액트가아닌 직접제어하는 것은 리액트에서 위험할 수 있다. 값을 가져오는 정도로 사용은 괜찮지만 변경하는것은 위험하다.
+        //enteredValue.current.value='';
+        setEnteredName('');
+    };
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className='form-control'>
+                <label htmlFor='name'>Your Name</label>
+                <input ref={nameInputRef}
+                       type='text'
+                       id='name'
+                       onChange={nameInputChangeHandler}
+                       value={enteredName}/>
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
