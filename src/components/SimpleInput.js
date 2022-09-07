0ae5cf8b2394767222f339624fc4c26f2e4f2601
qpakr38 <zczc0819@gmail.com>
2022-09-07 09:38:37 +0900

    REFACTORY : useRef 사용X

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,7 +1,6 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 
 const SimpleInput = (props) => {
-    const nameInputRef = useRef();
     const [enteredName, setEnteredName] = useState('');
     const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
@@ -34,10 +33,6 @@ const SimpleInput = (props) => {
         setEnteredNameTouched(true);
 
         console.log(enteredName)
-        const enteredValue = nameInputRef.current.value;
-        console.log(enteredValue)
-        //DOM객체를 리액트가아닌 직접제어하는 것은 리액트에서 위험할 수 있다. 값을 가져오는 정도로 사용은 괜찮지만 변경하는것은 위험하다.
-        //enteredValue.current.value='';
         setEnteredName('');
     };
     const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
@@ -49,8 +44,7 @@ const SimpleInput = (props) => {
         <form onSubmit={formSubmissionHandler}>
             <div className={nameInputClasses}>
                 <label htmlFor='name'>Your Name</label>
-                <input ref={nameInputRef}
-                       type='text'
+                <input type='text'
                        id='name'
                        onChange={nameInputChangeHandler}
                        onBlur={nameInputBlurHandler}
